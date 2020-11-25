cb5bbf544b5518957dee06018d93392f3f66dfda
Brian Wang <brrian@gmail.com>
2020-11-25 16:31:46 -0600

    Include categories in group query

--- src/graphql/group/groupTypeDefs.ts
+++ src/graphql/group/groupTypeDefs.ts
@@ -2,8 +2,10 @@ import { gql } from 'apollo-server-lambda';
 
 const groupTypeDefs = gql`
   type Group {
+    categories: JSON
     id: String
     name: String
+    runningBalance: Int
   }
 
   extend type Query {
