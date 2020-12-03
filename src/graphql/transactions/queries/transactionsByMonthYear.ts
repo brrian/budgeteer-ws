428ab192e06f54e363aa203eb59c0d6236aca2bc
Brian Wang <brrian@gmail.com>
2020-12-03 16:24:08 -0600

    Add query for transactions by month and year (#9)

---
+++ src/graphql/transactions/queries/transactionsByMonthYear.ts
@@ -0,0 +1,26 @@
+import { FieldResolver } from '../../models';
+import getMonthYearTimestamp from '../../util/getMonthYearTimestamp';
+import fetchTransactionsByRange from '../util/fetchTransactionsByRange';
+
+interface TransactionsByMonthYearArgs {
+  month: number;
+  year: number;
+}
+
+const transactionsByMonthYear: FieldResolver<TransactionsByMonthYearArgs> = (
+  _source,
+  { month, year },
+  context
+) => {
+  const timestamp = getMonthYearTimestamp(month, year);
+
+  const startOfMonth = new Date(timestamp);
+  const endOfMonth = new Date(year, month, 0);
+
+  const start = startOfMonth.toISOString().substring(0, 10);
+  const end = endOfMonth.toISOString().substring(0, 10);
+
+  return fetchTransactionsByRange(context.user.groupId, start, end);
+};
+
+export default transactionsByMonthYear;
--- src/graphql/transactions/transactionsResolvers.ts
+++ src/graphql/transactions/transactionsResolvers.ts
@@ -1,9 +1,11 @@
 import importTransactions from './mutations/importTransactions';
 import transactionsByDateRange from './queries/transactionsByDateRange';
+import transactionsByMonthYear from './queries/transactionsByMonthYear';
 
 export default {
   Query: {
     transactionsByDateRange,
+    transactionsByMonthYear,
   },
   Mutation: {
     importTransactions,
